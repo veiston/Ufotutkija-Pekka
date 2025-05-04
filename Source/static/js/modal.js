125a60a763edaeda882e5e76a548b6e89b34fd84
veiston <veikka.liukkonen@gmail.com>
2025-05-05 00:19:09 +0300

    Changed Alerts and prompts to custom modals. Added backround music for each scene.

---
+++ Source/static/js/modal.js
@@ -0,0 +1,41 @@
+function showModal(options) {
+  var modal = document.getElementById("modalBox");
+  var msg = document.getElementById("modalBoxMessage");
+  var btns = document.getElementById("modalBoxButtons");
+  msg.innerHTML = options.message;
+  btns.innerHTML = "";
+  var result = null;
+  options.buttons.forEach(function (btnOpt) {
+    var btn = document.createElement("button");
+    btn.textContent = btnOpt.label;
+    btn.className = btnOpt.className || "button";
+    btn.onclick = function () {
+      result = btnOpt.value;
+      modal.style.display = "none";
+      if (typeof options.onClose === "function") {
+        options.onClose(result);
+      }
+    };
+    btns.appendChild(btn);
+  });
+  modal.style.display = "flex";
+}
+window.showModal = showModal;
+// setTimeout(function () {
+/*    showModal({
+        message: "This flight costs $100. Do you want to proceed?",
+        buttons: [
+          { label: "Yes", value: true },
+          { label: "No", value: false },
+        ],
+        onClose: function (confirmed) {
+          if (confirmed) {
+            handleAirportClick(code);
+          } else {
+            map.removeLayer(flightLine);
+          }
+        },
+      });
+    }, 500); */
+
+// showInfoModal("Your message to the user here");
---
+++ Start Game.bat
@@ -0,0 +1,6 @@
+cd Source
+
+python server.py
+timeout 1
+start "" "http://127.0.0.1:5000"
+
