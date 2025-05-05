e6ee236a8358b7d814a06f308d297c0682b0b794
Ксения Шлёнская <k.shlenskaya@duotek.ru>
2025-05-05 11:11:21 +0300

    аdd inventory functionality

--- Source/inventory.py
+++ Source/inventory.py
@@ -2,37 +2,90 @@ import config
 from utilities import print_separator, input_press_enter, input_integer, yes_no
 from notifications import get_messages
 from database import *
-# from player import get_current_player
 from player import Player
 from colorama import Fore, Style
 
+# This is the new class Inventory that is now used for working with the inventory
+# Below it are the old functions, which might still be used in combat.py. Ideally they should later be added to the class as well
 
-def get_inventory():
-    player = Player()
-    player.id = Player.current_id
+class Inventory:
+    def __init__(self):
+        self.player = Player()
+        self.player.id = Player.current_id
 
-    if config.tossNokia:
+    def get_inventory(self):
         query = (f'SELECT inventory.item, inventory.amount, items.price, items.item_power, items.item_type, items.description '
-                f'FROM inventory '
-                f'INNER JOIN items '
-                f'ON inventory.item = items.name '
-                f'WHERE player_id = {player.id} '
-                f'AND inventory.item NOT IN ("Nokia");')
-    else:
-        query = (f'SELECT inventory.item, inventory.amount, items.price, items.item_power, items.item_type, items.description '
-                f'FROM inventory '
-                f'INNER JOIN items '
-                f'ON inventory.item = items.name '
-                f'WHERE player_id = {player.id};')
-    result = get_data_from_database(query)
-    if result:
-        columns = ["name", "amount", "price", "power", "type", "description"]
-        inventory = []
-        for oi in range(len(result)):
-            inventory.append({columns[i]: result[oi][i] for i in range(len(columns))})
-        return inventory
-    else:
-        return {}
+                 f'FROM inventory '
+                 f'INNER JOIN items '
+                 f'ON inventory.item = items.name '
+                 f'WHERE player_id = {self.player.id};')
+        result = get_data_from_database(query)
+        if result:
+            columns = ["name", "amount", "price", "power", "type", "description"]
+            inventory = []
+            for oi in range(len(result)):
+                inventory.append({columns[i]: result[oi][i] for i in range(len(columns))})
+            return inventory
+        else:
+            return {}
+
+    def add_item(self, item_name):
+        inventory = self.get_inventory()
+        for entry in inventory:
+            if entry['name'] == item_name:
+                new_amount = entry['amount'] + 1
+                update_data_in_database(
+                    f'UPDATE inventory SET amount = {new_amount} WHERE player_id = {self.player.id} AND item = "{item_name}"'
+                )
+                return self.get_inventory()
+
+        update_data_in_database(
+            f'INSERT INTO inventory(player_id, item, amount) VALUES ({self.player.id}, "{item_name}", 1);'
+        )
+        return self.get_inventory()
+
+    def delete_item(self, item_name):
+        inventory = self.get_inventory()
+        for entry in inventory:
+            if entry['name'] == item_name:
+                if entry['amount'] > 1:
+                    new_amount = entry['amount'] - 1
+                    update_data_in_database(
+                        f'UPDATE inventory SET amount = {new_amount} WHERE player_id = {self.player.id} AND item = "{item_name}";'
+                    )
+                elif entry['amount'] == 1:
+                    update_data_in_database(
+                        f'DELETE FROM inventory WHERE player_id = {self.player.id} AND item = "{item_name}";'
+                    )
+                return self.get_inventory()
+
+
+# def get_inventory():
+#     player = Player()
+#     player.id = Player.current_id
+#
+#     if config.tossNokia:
+#         query = (f'SELECT inventory.item, inventory.amount, items.price, items.item_power, items.item_type, items.description '
+#                 f'FROM inventory '
+#                 f'INNER JOIN items '
+#                 f'ON inventory.item = items.name '
+#                 f'WHERE player_id = {player.id} '
+#                 f'AND inventory.item NOT IN ("Nokia");')
+#     else:
+#         query = (f'SELECT inventory.item, inventory.amount, items.price, items.item_power, items.item_type, items.description '
+#                 f'FROM inventory '
+#                 f'INNER JOIN items '
+#                 f'ON inventory.item = items.name '
+#                 f'WHERE player_id = {player.id};')
+#     result = get_data_from_database(query)
+#     if result:
+#         columns = ["name", "amount", "price", "power", "type", "description"]
+#         inventory = []
+#         for oi in range(len(result)):
+#             inventory.append({columns[i]: result[oi][i] for i in range(len(columns))})
+#         return inventory
+#     else:
+#         return {}
 
 
 def list_inventory(): #mainly for combat-purposes, but it might be useful elsewhere too
@@ -83,22 +136,26 @@ def list_inventory(): #mainly for combat-purposes, but it might be useful elsewh
             print("There's no such option")
             input_press_enter('')
 
+#
+# def update_inventory(value, player_id, item):
+#     amount = 0
+#     for i in get_inventory(): #search database for the item and how many you have
+#         if i['name'] == item:
+#             amount = i['amount']
+#
+#     if amount == 0:
+#         update_data_in_database(f'INSERT INTO inventory(player_id, item, amount) VALUES ({player_id}, "{item}", {value});')
+#
+#     elif amount+value<0:
+#         print("woah hey you don't have that many items!")
+#         return 'nope'
+#
+#     elif amount+value==0:
+#         update_data_in_database(f'DELETE FROM inventory WHERE player_id = {player_id} AND item = "{item}";')
+#
+#     else:
+#         update_data_in_database(f'UPDATE inventory SET amount = {amount+value} WHERE player_id = {player_id} AND item = "{item}"')
+#
+#
 
-def update_inventory(value, player_id, item):
-    amount = 0
-    for i in get_inventory(): #search database for the item and how many you have
-        if i['name'] == item:
-            amount = i['amount']
 
-    if amount == 0:
-        update_data_in_database(f'INSERT INTO inventory(player_id, item, amount) VALUES ({player_id}, "{item}", {value});')
-
-    elif amount+value<0:
-        print("woah hey you don't have that many items!")
-        return 'nope'
-
-    elif amount+value==0:
-        update_data_in_database(f'DELETE FROM inventory WHERE player_id = {player_id} AND item = "{item}";')
-
-    else:
-        update_data_in_database(f'UPDATE inventory SET amount = {amount+value} WHERE player_id = {player_id} AND item = "{item}"')
--- Source/inventory_routes.py
+++ Source/inventory_routes.py
@@ -1,7 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from inventory import Inventory
 
 inventory_routes = Blueprint('inventory_routes', __name__)
 
-@inventory_routes.route('/inventory/example-route-name')
-def inventory_example_route_name():
-    return {'example_field': 'example response'}
+@inventory_routes.route('/inventory/items', methods=['GET'])
+def get_inventory_items():
+    inventory = Inventory()
+    items = inventory.get_inventory()
+    return jsonify(items)
+
+@inventory_routes.route('/inventory/add', methods=['POST'])
+def add_inventory_item():
+    inventory = Inventory()
+    data = request.get_json()
+    item = data.get('item')
+    updated_inventory = inventory.add_item(item)
+    return jsonify(updated_inventory)
+
+@inventory_routes.route('/inventory/delete', methods=['POST'])
+def delete_inventory_item():
+    inventory = Inventory()
+    data = request.get_json()
+    item = data.get('item')
+    updated_inventory = inventory.delete_item(item)
+    return jsonify(updated_inventory)
+
