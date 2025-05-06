6fb41c2daa978a56dc57f68819536c8beff363be
Ксения Шлёнская <k.shlenskaya@duotek.ru>
2025-05-06 20:04:35 +0300

    add fade-in-audio script for background audio

---
+++ Source/static/js/fade-in-audio.js
@@ -0,0 +1,19 @@
+const fadeAudio = document.querySelector('#fadeAudio');
+fadeAudio.volume = 0;
+fadeAudio.muted = false;
+
+const fadeInAudio = (audioElement, targetVolume = 1, duration = 8000) => {
+    audioElement.volume = 0;
+    const step = 0.01;
+    const intervalTime = duration * step / targetVolume;
+
+    const fadeInterval = setInterval(() => {
+        if (audioElement.volume < targetVolume) {
+            audioElement.volume = Math.min(audioElement.volume + step, targetVolume);
+        } else {
+            clearInterval(fadeInterval);
+        }
+    }, intervalTime);
+}
+
+fadeInAudio(fadeAudio);
